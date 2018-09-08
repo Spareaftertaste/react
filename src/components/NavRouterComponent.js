10cf51693e88c1b00cecfa71edcd35c4a1613b96
Spareaftertaste <2383860945@qq.com>
2018-09-08 21:35:34 +0800

    nav+轮播图。下面开始下面的内容部分

---
+++ src/components/NavRouterComponent.js
@@ -0,0 +1,114 @@
+'use strict';
+
+import React from 'react';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+
+
+require('styles//NavRouter.css');
+
+
+class NavRouterComponent extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <div>
+            {/*这里是一级nav*/}
+            <div  className={'first-nav flex'}>
+              <div className={'logo'}>
+                <div></div>
+              </div>
+              <NavBtn activeOnlyWhenExact={true} to="/" label="发现音乐" />
+              <NavBtn to="/about" label="我的音乐" />
+              <NavBtn to="/topics" label="朋友" />
+              <div className={'fnavbtn'}>
+                <a href="https://music.163.com/store/product" target="_blank">商城</a>
+              </div>
+              <div className={'fnavbtn'}>
+                <a href="https://music.163.com/nmusician/web/index" target="_blank">音乐人</a>
+              </div>
+              <div className={'fnavbtn'}>
+                <a href="https://music.163.com/#/download" target="_blank">下载客户端</a>
+                <span className={'fnavbtn-hot'}></span>
+              </div>
+              <div className={'fnavbtn-search absolute-center'}>
+                <input type="text" placeholder={'音乐/视频/电台/用户'} />
+                <span className={'search-logo'}></span>
+              </div>
+            </div>
+          </div>
+          {/*这里是二级nav*/}
+          <div className={'second-nav'}>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={Mine} />
+            <Route path="/topics" component={Friend} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+// navbtn单独拆出来，用户选中时样式相应的改变
+const NavBtn = ({ label, to, activeOnlyWhenExact }) => (
+  <Route
+    path={to}
+    exact={activeOnlyWhenExact}
+    children={({ match }) => (
+      <div className={match ? 'active fnavbtn' : 'fnavbtn'}>
+        {match ? (<span className="nav-triangle"></span>) : ''}
+        <Link to={to}>{label}</Link>
+      </div>
+    )}
+  />
+);
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const Mine = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Friend = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+
+// NavRouterComponent.displayName = 'NavRouterComponent';
+
+// Uncomment properties you need
+// NavRouterComponent.propTypes = {};
+// NavRouterComponent.defaultProps = {};
+
+export default NavRouterComponent;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'core-js/fn/object/assign';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './components/Main';
+import NavRouterComponent from './components/NavRouterComponent';
+
+// Render the main component into the dom
+ReactDOM.render(<NavRouterComponent text={'simida'} />, document.getElementById('app'));
